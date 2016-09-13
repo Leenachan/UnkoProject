f05c3aef9b33cca606cc4bc45383ec8d8999168b
Leenachan <pandacoro.leena314@gmail.com>
2016-09-13 17:57:22 +0900

    うんこ発射
    連射できるようにした。

---
+++ Assets/gameManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gameManager : MonoBehaviour
+{
+
+    public GameObject objSphere;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("mouse 0"))
+        {
+            GameObject.Instantiate(objSphere, new Vector3(205, 209, 116), Quaternion.identity);
+        }
+    }
+}
